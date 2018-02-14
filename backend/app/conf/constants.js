fac60894d79abf64bc5e053a375914a517c02162
Halil AYYILDIZ <halil.ayyildiz@gmail.com>
2018-02-14 21:44:46 +0300

    api bug workaround fix

--- backend/app/conf/constants.js
+++ backend/app/conf/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    AHASHPOOL_WALLET_SERVICE_PREFIX: "https://www.ahashpool.com/api/walletEx/?address=",
+    AHASHPOOL_WALLET_SERVICE_PREFIX: "https://www.ahashpool.com/api/walletEx/?address=%20",
     WALLET_EARNING_BTC_DEAFULT_PRECISION_NUM: 6,
     WALLET_EARNING_USD_DEAFULT_PRECISION_NUM: 2
 });
